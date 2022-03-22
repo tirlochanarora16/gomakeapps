af49ecd7640035e97aec5382fe3a3686ba19f740
tirlochanarora16 <tirlochan.arora16@gmail.com>
2022-03-22 12:44:18 +0530

    updated the refrence section

--- components/Refrence/Refrence.js
+++ components/Refrence/Refrence.js
@@ -9,6 +9,8 @@ import phone1 from "../../images/refrence/phone1.png";
 import phone2 from "../../images/refrence/phone2.png";
 import phone3 from "../../images/refrence/phone3.png";
 import phone4 from "../../images/refrence/phone4.png";
+
+import ruler from "../../images/refrence/ruler.png";
 import logo from "../../images/navigation/logo.png";
 
 import style from "./Refrence.module.scss";
@@ -17,7 +19,9 @@ const Refrence = () => {
   return (
     <section className={style.refrence}>
       <div className={style["refrence__head"]}>
-        <RiPencilRuler2Fill />
+        <div className={style["refrence__head--img"]}>
+          <Image src={ruler} width={45} height={45} />
+        </div>
         <h1>NOS CAS D&#39;ÉTUDES DE RÉFÉRENCE</h1>
       </div>
       <div className={style["refrence__container"]}>
@@ -26,17 +30,27 @@ const Refrence = () => {
         </div>
         <div className={style["refrence__container--display"]}>
           <div className={style["refrence__container--display-img"]}>
-            <Image src={banner} alt="banner img" />
+            <Image src={banner} alt="banner img" width={150} height={40} />
           </div>
           <div className={style["refrence__container--display-title"]}>
             <p>JUDGE-FOOD Advisor App</p>
           </div>
-          <Image src={logo} alt="go make apps logo" width={89} height={121} />
+          <div className={style["refrence__container--display-logo"]}>
+            <Image src={logo} alt="go make apps logo" width={89} height={100} />
+          </div>
           <div className={style["refrence__container--display-images"]}>
-            <Image src={phone1} alt="phone 1" />
-            <Image src={phone2} alt="phone 2" />
-            <Image src={phone3} alt="phone 3" />
-            <Image src={phone4} alt="phone 4" />
+            <div>
+              <Image src={phone1} alt="phone 1" />
+            </div>
+            <div>
+              <Image src={phone2} alt="phone 2" />
+            </div>
+            <div>
+              <Image src={phone3} alt="phone 3" />
+            </div>
+            <div>
+              <Image src={phone4} alt="phone 4" />
+            </div>
           </div>
         </div>
         <div className={style["refrence__button--front"]}>

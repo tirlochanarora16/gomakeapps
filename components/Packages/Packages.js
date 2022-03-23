cc07e9637845c4622bfcad72f8d3fc5f0babc948
tirlochanarora16 <tirlochan.arora16@gmail.com>
2022-03-23 20:33:28 +0530

    added the packages section

--- components/Packages/Packages.js
+++ components/Packages/Packages.js
@@ -1,20 +1,116 @@
 import Image from "next/image";
+import Link from "next/link";
+
 import thunder from "../../images/icons/thunder.png";
+import monitor from "../../images/icons/monitor.svg";
+import monitorLight from "../../images/icons/monitor-light.png";
+import smartphone from "../../images/icons/smartphone.svg";
+import smartphoneLight from "../../images/icons/smartphone-light.png";
+import smartwatch from "../../images/icons/smartwatch.svg";
+
+import { premiumPackageItems, privilegeAndunlimitedPackageItems } from "./data";
 
 import style from "./Packages.module.scss";
 
-const PackagesItem = (props) => {
+const PackagesListItem = (props) => {
+  const colorStyle = {
+    color:
+      props.package === "unlimited"
+        ? "#f2f2f2"
+        : props.package === "privilege"
+        ? "#000"
+        : "",
+  };
   return (
-    <div className={style["packages__item"]}>
-      <div className={style["packages__item--name"]}>
-        <h2>Premium</h2>
+    <div className={style["packages__item--list-item"]}>
+      <div
+        className={style["packages__item--list-icon"]}
+        style={{
+          filter: props.package === "privilege" ? "brightness(0%)" : "",
+        }}
+      >
+        <Image src={props.icon} width={20} height={20} />
       </div>
-      <div className={style["packages__item--amount"]}>
-        <div className={style["packages__item--amount-monthly"]}>
-          <p>895,00 € / mois 1 an d'engagement</p>
+      <div className={style["packages__item--list-text"]}>
+        <p style={colorStyle}>{props.text}</p>
+      </div>
+    </div>
+  );
+};
+
+//895,00 € / mois, 1 an d'engagement, ou 8990,00 € en une seule fois
+
+const PackagesItem = (props) => {
+  const styles = {
+    backgroundColor: props.packageName === "privilege" ? "#0141f8" : "",
+    backgroundColor: props.packageName === "unlimited" ? "#000" : "",
+    marginTop: props.packageName === "unlimited" ? "1rem" : "",
+  };
+
+  const colorStyle = {
+    color:
+      props.packageName === "unlimited"
+        ? "#f2f2f2"
+        : props.packageName === "privilege"
+        ? "#000"
+        : "",
+  };
+
+  return (
+    <div
+      className={style["packages__item"]}
+      style={{
+        backgroundColor: props.packageName === "unlimited" ? "#000" : "",
+      }}
+      key={props.id}
+    >
+      <div className={style["packages__item--name"]} style={styles}>
+        <h2>{props.packageName}</h2>
+      </div>
+      <div className={style["packages__item--container"]}>
+        <div className={style["packages__item--amount"]}>
+          <div className={style["packages__item--amount-monthly"]}>
+            <p style={colorStyle}>{props.monthlyAmount}</p>
+            <p style={colorStyle}>{props.timePeriod}t</p>
+          </div>
+          <div className={style["packages__item--amount-once"]}>
+            <p style={colorStyle}>{props.oneTimeAmount}</p>
+          </div>
+          <div
+            className={style["packages__item--amount-divider"]}
+            style={{
+              backgroundColor:
+                props.packageName === "unlimited" ? "#f2f2f2" : "",
+            }}
+          ></div>
         </div>
-        <div className={style["packages__item--amount-once"]}>
-          <p>ou 8990,00 € en une seule fois</p>
+        <div className={style["packages__item--list"]}>
+          {props.packageItems.map((item) => (
+            <PackagesListItem
+              package={props.packageName}
+              icon={item.icon}
+              text={item.text}
+              key={item.id}
+            />
+          ))}
+        </div>
+        <div
+          className={style["packages__item-button"]}
+          style={{
+            color:
+              props.packageName === "privilege" ||
+              props.packageName === "unlimited"
+                ? "#000"
+                : "",
+            backgroundColor: props.packageName === "unlimited" ? "#fff" : "",
+          }}
+        >
+          <Link href="#">Buy Now</Link>
+        </div>
+        <div className={style["packages__item-icons"]}>
+          {props.icons.map((icon) => (
+            <Image src={icon.icon} width={25} height={25} />
+          ))}
         </div>
       </div>
     </div>
@@ -33,7 +129,40 @@ const Packages = () => {
         </h1>
       </div>
       <div className={style["packages__container"]}>
-        {/* <PackagesItem /> */}
+        <PackagesItem
+          id={1}
+          packageName="premium"
+          monthlyAmount="895,00 € / mois"
+          timePeriod="1 an d'engagement"
+          oneTimeAmount="ou 8990,00 € en une seule fois"
+          packageItems={premiumPackageItems}
+          icons={[{ id: 1, icon: monitor.src }]}
+        />
+        <PackagesItem
+          id={2}
+          packageName="privilege"
+          monthlyAmount="4500,00 € / mois"
+          timePeriod="1 an d'engagement"
+          oneTimeAmount="ou 49 500,00 € en une seule fois"
+          packageItems={premiumPackageItems}
+          icons={[
+            { id: 1, icon: monitor.src },
+            { id: 2, icon: smartphone.src },
+          ]}
+        />
+        <PackagesItem
+          id={3}
+          packageName="unlimited"
+          monthlyAmount="8500,00 € / mois"
+          timePeriod="1 an d'engagement"
+          oneTimeAmount="ou 99 500,00 € en une seule fois"
+          packageItems={privilegeAndunlimitedPackageItems}
+          icons={[
+            { id: 1, icon: monitorLight.src },
+            { id: 2, icon: smartphoneLight.src },
+            { id: 3, icon: smartwatch.src },
+          ]}
+        />
       </div>
     </section>
   );
---
+++ components/Packages/data.js
@@ -0,0 +1,90 @@
+import ruler from "../../images/icons/ruler.svg";
+import rulerLight from "../../images/icons/ruler-light.png";
+import settings from "../../images/icons/settings.svg";
+import settingsLight from "../../images/icons/settings-light.png";
+import shield from "../../images/icons/shield.svg";
+import responsive from "../../images/icons/responsive.svg";
+import ranking from "../../images/icons/ranking.svg";
+import technology from "../../images/icons/technology.svg";
+import clapperboard from "../../images/icons/clapperboard.svg";
+import clapperboardLight from "../../images/icons/clapperboard-light.png";
+import optionLight from "../../images/icons/option-light.png";
+import codingLight from "../../images/icons/coding-light.png";
+import programmingLight from "../../images/icons/programming-light.png";
+import websiteLight from "../../images/icons/website-light.png";
+
+export const premiumPackageItems = [
+  {
+    id: 1,
+    icon: ruler.src,
+    text: "Conception d'un site vitrine sur mesure et personnalisé réalisable en moins de 24h*",
+  },
+  {
+    id: 2,
+    icon: settings.src,
+    text: "Maintenance anuelle et garantie de fonctionnement assuré",
+  },
+  {
+    id: 3,
+    icon: shield.src,
+    text: "Installation des protocoles de sécurité HTTPS/SLL",
+  },
+  {
+    id: 4,
+    icon: responsive.src,
+    text: "Site internet Responsive s'adaptant aux écrans de smartphones, tablette et PC",
+  },
+  {
+    id: 5,
+    icon: ranking.src,
+    text: "Référencement SEO struc-turelles",
+  },
+  {
+    id: 6,
+    icon: technology.src,
+    text: "Programme dernières technologies",
+  },
+  {
+    id: 7,
+    icon: clapperboard.src,
+    text: "Production de vidéo pro-essionnelle",
+  },
+];
+
+export const privilegeAndunlimitedPackageItems = [
+  {
+    id: 1,
+    icon: rulerLight.src,
+    text: "Conception d'un site vitrine sur mesure et personnalisé réalisable en moins de 24h*",
+  },
+  {
+    id: 2,
+    icon: websiteLight.src,
+    text: "Jusqu'à 2 Plateforme en ligne toutes fonctionnalités intégré et créées sur mesure.",
+  },
+  {
+    id: 3,
+    icon: optionLight.src,
+    text: "Jusqu'à 3 Application mobile iOS ET Android compris",
+  },
+  {
+    id: 4,
+    icon: codingLight.src,
+    text: "Panneau d'administration Dashboard Admin en ligne",
+  },
+  {
+    id: 5,
+    icon: settingsLight.src,
+    text: "Maintenance annuelle et garantie de fonctionnement sur l'ensemble des systèmes",
+  },
+  {
+    id: 6,
+    icon: programmingLight.src,
+    text: "PDéveloppement 100% flexible",
+  },
+  {
+    id: 7,
+    icon: clapperboardLight.src,
+    text: "Production de vidéo pro-essionnelle",
+  },
+];
